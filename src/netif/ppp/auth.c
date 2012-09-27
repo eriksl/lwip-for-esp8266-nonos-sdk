2cedfd1ab8ed111d2387622306402c013fe0cecc
Sylvain Rochet <gradator@gradator.net>
2012-09-27 23:53:20 +0200

    PPP, changed all the code enclosed between PPP_SERVER #if macro to our PPP PCB structure, making it easier to support PPP server in the future

--- src/netif/ppp/auth.c
+++ src/netif/ppp/auth.c
@@ -439,28 +439,19 @@ link_down(int unit)
  * The link is established.
  * Proceed to the Dead, Authenticate or Network phase as appropriate.
  */
-void
-link_established(int unit)
-{
-  int auth;
-  int i;
-  struct protent *protp;
-  lcp_options *wo = &lcp_wantoptions[unit];
-  lcp_options *go = &lcp_gotoptions[unit];
-#if PAP_SUPPORT || CHAP_SUPPORT
-  lcp_options *ho = &lcp_hisoptions[unit];
-#endif /* PAP_SUPPORT || CHAP_SUPPORT */
+void link_established(ppp_pcb *pcb) {
+    int auth;
+#if PPP_SERVER
+    lcp_options *wo = &pcb->lcp_wantoptions;
+    lcp_options *go = &pcb->lcp_gotoptions;
+#endif /* PPP_SERVER */
+    lcp_options *ho = &pcb->lcp_hisoptions;
+    int i;
+    struct protent *protp;
+#if PPP_SERVER
+    int errcode;
+#endif /* PPP_SERVER */
 
-  AUTHDEBUG(LOG_INFO, ("link_established: unit %d; Lowering up all protocols...\n", unit));
-  /*
-   * Tell higher-level protocols that LCP is up.
-   */
-  for (i = 0; (protp = ppp_protocols[i]) != NULL; ++i) {
-    if (protp->protocol != PPP_LCP && protp->enabled_flag && protp->lowerup != NULL) {
-      (*protp->lowerup)(unit);
-    }
-  }
-  if (ppp_settings.auth_required && !(go->neg_chap || go->neg_upap)) {
     /*
      * We wanted the peer to authenticate itself, and it refused:
      * treat it as though it authenticated with PAP using a username
@@ -473,8 +464,58 @@ link_established(int unit)
     }
   }
 
-  lcp_phase[unit] = PHASE_AUTHENTICATE;
-  auth = 0;
+#if PPP_SERVER
+#if PPP_ALLOWED_ADDRS
+    if (!auth_required && noauth_addrs != NULL)
+	set_allowed_addrs(unit, NULL, NULL);
+#endif /* PPP_ALLOWED_ADDRS */
+
+    if (pcb->settings.auth_required && !(0
+#if PAP_SUPPORT
+	|| go->neg_upap
+#endif /* PAP_SUPPORT */
+#if CHAP_SUPPORT
+	|| go->neg_chap
+#endif /* CHAP_SUPPORT */
+#if EAP_SUPPORT
+	|| go->neg_eap
+#endif /* EAP_SUPPORT */
+	)) {
+
+#if PPP_ALLOWED_ADDRS
+	/*
+	 * We wanted the peer to authenticate itself, and it refused:
+	 * if we have some address(es) it can use without auth, fine,
+	 * otherwise treat it as though it authenticated with PAP using
+	 * a username of "" and a password of "".  If that's not OK,
+	 * boot it out.
+	 */
+	if (noauth_addrs != NULL) {
+	    set_allowed_addrs(unit, NULL, NULL);
+	} else
+#endif /* PPP_ALLOWED_ADDRS */
+	if (!wo->neg_upap || !pcb->settings.null_login) {
+	    ppp_warn("peer refused to authenticate: terminating link");
+#if 0 /* UNUSED */
+	    status = EXIT_PEER_AUTH_FAILED;
+#endif /* UNUSED */
+	    errcode = PPPERR_AUTHFAIL;
+	    ppp_ioctl(pcb, PPPCTLS_ERRCODE, &errcode);
+	    lcp_close(pcb, "peer refused to authenticate");
+	    return;
+	}
+    }
+#endif /* PPP_SERVER */
+
+    new_phase(pcb, PHASE_AUTHENTICATE);
+    auth = 0;
+#if PPP_SERVER
+#if EAP_SUPPORT
+    if (go->neg_eap) {
+	eap_authpeer(pcb, pcb->settings.our_name);
+	auth |= EAP_PEER;
+    } else
+#endif /* EAP_SUPPORT */
 #if CHAP_SUPPORT
   if (go->neg_chap) {
     ChapAuthPeer(unit, ppp_settings.our_name, go->chap_mdtype);
@@ -567,16 +608,16 @@ network_phase(int unit)
 /*
  * The peer has failed to authenticate himself using `protocol'.
  */
-void
-auth_peer_fail(int unit, u16_t protocol)
-{
-  LWIP_UNUSED_ARG(protocol);
-
-  AUTHDEBUG(LOG_INFO, ("auth_peer_fail: %d proto=%X\n", unit, protocol));
-  /*
-   * Authentication failure: take the link down
-   */
-  lcp_close(unit, "Authentication failed");
+void auth_peer_fail(ppp_pcb *pcb, int protocol) {
+    int errcode = PPPERR_AUTHFAIL;
+    /*
+     * Authentication failure: take the link down
+     */
+#if 0 /* UNUSED */
+    status = EXIT_PEER_AUTH_FAILED;
+#endif /* UNUSED */
+    ppp_ioctl(pcb, PPPCTLS_ERRCODE, &errcode);
+    lcp_close(pcb, "Authentication failed");
 }
 
 
@@ -602,22 +643,12 @@ auth_peer_success(int unit, u16_t protocol, char *name, int namelen)
       return;
   }
 
-  /*
-   * Save the authenticated name of the peer for later.
-   */
-  if (namelen > (int)sizeof(peer_authname) - 1) {
-    namelen = sizeof(peer_authname) - 1;
-  }
-  BCOPY(name, peer_authname, namelen);
-  peer_authname[namelen] = 0;
-  
-  /*
-   * If there is no more authentication still to be done,
-   * proceed to the network (or callback) phase.
-   */
-  if ((auth_pending[unit] &= ~pbit) == 0) {
-    network_phase(unit);
-  }
+    /*
+     * If there is no more authentication still to be done,
+     * proceed to the network (or callback) phase.
+     */
+    if ((pcb->auth_pending &= ~bit) == 0)
+        network_phase(pcb);
 }
 
 /*
