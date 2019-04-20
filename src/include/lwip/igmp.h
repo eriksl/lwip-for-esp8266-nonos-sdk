c3d4b7848046b4c54c2ae89a3e78028fca82fb8b
Erik Slagter <erik@openpli.org>
2019-04-20 20:01:00 +0200

    ESP: Espressif eagle random number generator integration.

--- src/include/lwip/igmp.h
+++ src/include/lwip/igmp.h
@@ -96,7 +96,10 @@ void   igmp_input(struct pbuf *p, struct netif *inp, const ip4_addr_t *dest);
 err_t  igmp_joingroup(const ip4_addr_t *ifaddr, const ip4_addr_t *groupaddr);
 err_t  igmp_leavegroup(const ip4_addr_t *ifaddr, const ip4_addr_t *groupaddr);
 void   igmp_tmr(void);
-
+// Espressif code
+int    r_rand(void);
+#undef LWIP_RAND
+#define LWIP_RAND()  r_rand()
 #ifdef __cplusplus
 }
 #endif
