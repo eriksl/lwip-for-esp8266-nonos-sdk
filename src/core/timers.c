338206c6e9f870fae856de459e8800964ce0b03f
Erik Slagter <erik@openpli.org>
2019-04-20 19:13:36 +0200

    ESP: Hooks for (Espressif) contributed DHCP server.

--- src/core/timers.c
+++ src/core/timers.c
@@ -157,6 +157,8 @@ dhcp_timer_coarse(void *arg)
   LWIP_UNUSED_ARG(arg);
   LWIP_DEBUGF(TIMERS_DEBUG, ("tcpip: dhcp_coarse_tmr()\n"));
   dhcp_coarse_tmr();
+// Espressif code
+  dhcps_coarse_tmr();
   sys_timeout(DHCP_COARSE_TIMER_MSECS, dhcp_timer_coarse, NULL);
 }
 
